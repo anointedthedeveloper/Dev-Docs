432d6a80996cf7916ab18aaaa9460c99ea86c4c7
Anointedthedeveloper <anointedthedeveloper@gmail.cp>
2026-02-01 09:27:54 +0100

    feat: add Head component for Google AdSense integration

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      {/* Google AdSense meta tag */}
+      <meta name="google-adsense-account" content="ca-pub-1727042318749362" />
+
+      {/* Google AdSense script */}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1727042318749362"
+        crossOrigin="anonymous"
+      />
+    </>
+  );
+}
